09ce5fa48f49a6f24abb223339c4595aa162721b
Jpezz <jpezzullo1@gmail.com>
2021-12-22 00:51:05 -0500

    added a few animations for RoboCapo, integrated the walk cycle, and added hurtboxes for Robocapo

---
+++ Assets/Models/Boss_RoboCapo/Scripts/RC_Hurtbox.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RC_Hurtbox : MonoBehaviour
+{
+    [SerializeField]
+    bossAiRobocapo bossAiReference;
+    [SerializeField]
+    int hurtboxDamage;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Weapon")
+        {
+            bossAiReference.bossHealth -= hurtboxDamage;
+        }
+    }
+}
