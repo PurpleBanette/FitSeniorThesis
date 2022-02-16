23a335f13aeb0c5ca5294bcec0bf582aef01a01e
JOSEPH PEZZULLO <JOSEPH_PEZZULLO@fitsuny.edu>
2022-02-16 17:09:54 -0500

    fixed a bug that made RC try to do 4 seperate attacks at once and reduced the delay on his attacks, also gave Yasuke a cooler sword that nikko made, continued development of the RC boss fight

--- Assets/RC_idleBehavior.cs
+++ Assets/RC_idleBehavior.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class resetWindUp : StateMachineBehaviour
+public class RC_idleBehavior : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        bossAiRobocapo.instance.playerTracking = true;    
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -19,7 +19,7 @@ public class resetWindUp : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        bossAiRobocapo.instance.bossAnimator.SetBool("WindUpOverhead",false);
+        bossAiRobocapo.instance.playerTracking = false;
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
--- Assets/reset3Hit.cs
+++ Assets/reset3Hit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class resetWindUp : StateMachineBehaviour
+public class reset3Hit : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -19,7 +19,7 @@ public class resetWindUp : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        bossAiRobocapo.instance.bossAnimator.SetBool("WindUpOverhead",false);
+        bossAiRobocapo.instance.bossAnimator.SetBool("3Hit", false);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
