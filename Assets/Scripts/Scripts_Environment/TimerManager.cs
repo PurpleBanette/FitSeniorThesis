6b23c521786ad8a57ce7cba8cef1e0cb7f475a29
Nikko Griffin <you@example.com>
2022-04-18 00:51:10 -0400

    YasukeMainMenuConnectedtoIntro

---
+++ Assets/Scripts/Scripts_Environment/TimerManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerManager
+{
+    [Header("Timer")]
+    
+    public static float mytimer = 204f;
+    public static bool switchedScene = false;
+
+    //when boss health reach XXX return to scene  into time
+    
+
+}
