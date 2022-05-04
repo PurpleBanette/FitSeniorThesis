6efbbc18eb6378dac8b74f851d9d80c3b0416584
NIKKO GRIFFIN <NikkoGriffin54@yahoo.com>
2022-05-04 09:44:50 -0400

    MainMenuto_RoboSetup

--- Assets/Scripts/Scripts_Environment/TimerManager.cs
+++ Assets/Scripts/Scripts_Environment/TimerManager.cs
@@ -1,21 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimerManager
 {
     [Header("Timer")]
     
-    public static float mytimer = 80f;
+    public static float mytimer = 308f;
     public static bool switchedScene = false;
 
     //when boss health reach XXX return to scene  into time
     void Update()
     {
-        if(bossAiRobocapoRemake.instance.bossHealth <= 0)
-        {
-
-        }
+       
     }
     
 
